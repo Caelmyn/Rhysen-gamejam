cc3bbcdece481b55c090db3fa05ed9ad967a05bf
ChessNeo <evonsheika@gmail.com>
2016-01-14 19:06:08 +0100

    HP Bar
    Little modification to keep things simple

--- Assets/Perso Assets/Test/HPBar/HpBar.cs
+++ Assets/Perso Assets/Test/HPBar/HpBar.cs
@@ -14,17 +14,29 @@ public class HpBar : MonoBehaviour
                 _percentLeft = 100;
             if (_percentLeft < 0)
                 _percentLeft = 0;
-            Debug.Log(_percentLeft);
             this.transform.localScale = new Vector3(_percentLeft / 100, 1, 1);
-            _texture.transform.localScale = new Vector3(1 / this.transform.localScale.x, 1, 1);
+            //_texture.transform.localScale = new Vector3(1 / this.transform.localScale.x, 1, 1);
+            _image.uvRect = new Rect(0, 0, this.transform.localScale.x, 1);
         }
     }
 
-    [SerializeField] private RectTransform _texture;
+    //[SerializeField] private RectTransform _texture;
+    private RawImage _image;
     private float _percentLeft;
 
     void Start()
     {
+        _image = GetComponent<RawImage>() as RawImage;
+        if (_image == null)
+            Debug.LogError("This component hasn't RawImage component attached");
         PercentLeft = 100;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            PercentLeft = -10;
+        if (Input.GetKeyDown(KeyCode.Z))
+            PercentLeft = 10;
     }
 }

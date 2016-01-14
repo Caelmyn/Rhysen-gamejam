b81ea136fa477da81cf125b7e93ae0f11fa10e57
ChessNeo <evonsheika@gmail.com>
2016-01-14 14:44:03 +0100

    Hp Bar
    Begin of the feature Hp Bar

---
+++ Assets/Perso Assets/Test/HPBar/HpBar.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HpBar : MonoBehaviour
+{
+    public int PercentLeft
+    {
+        get { return _percentLeft; }
+        set
+        {
+            _percentLeft += value;
+            if (_percentLeft > 100)
+                _percentLeft = 100;
+            if (_percentLeft < 0)
+                _percentLeft = 0;
+        }
+    }
+
+    [SerializeField] private RectTransform _texture;
+    private int _percentLeft;
+
+    void Start()
+    {
+    }
+}

7c71ae6129af3639beccba9875f1635b0732b664
ChessNeo <evonsheika@gmail.com>
2016-01-25 18:50:07 +0100

    HP Bar
    Repo cleanup, added a .gitignore file and some lil changes to HP bar
    code

--- Assets/Perso Assets/Test/HPBar/HpBar.cs
+++ Assets/Perso Assets/Test/HPBar/HpBar.cs
@@ -9,18 +9,16 @@ public class HpBar : MonoBehaviour
         get { return _percentLeft; }
         set
         {
-            _percentLeft += value;
+            _percentLeft = value;
             if (_percentLeft > 100)
                 _percentLeft = 100;
             if (_percentLeft < 0)
                 _percentLeft = 0;
             this.transform.localScale = new Vector3(_percentLeft / 100, 1, 1);
-            //_texture.transform.localScale = new Vector3(1 / this.transform.localScale.x, 1, 1);
             _image.uvRect = new Rect(0, 0, this.transform.localScale.x, 1);
         }
     }
-
-    //[SerializeField] private RectTransform _texture;
+    
     private RawImage _image;
     private float _percentLeft;
 
@@ -30,13 +28,5 @@ public class HpBar : MonoBehaviour
         if (_image == null)
             Debug.LogError("This component hasn't RawImage component attached");
         PercentLeft = 100;
-    }
-
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-            PercentLeft = -10;
-        if (Input.GetKeyDown(KeyCode.Z))
-            PercentLeft = 10;
     }
 }

30a41896ede75f8c655e3e8d23236dd44d04f39c
ChessNeo <evonsheika@gmail.com>
2016-02-15 12:26:41 +0100

    Back button and scrit to load scene

---
+++ Assets/Perso Assets/Scripts/LoadScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LoadScene : MonoBehaviour
+{
+    [SerializeField] private string _sceneName;
+
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Load();
+	}
+
+    public void Load()
+    {
+        SceneManager.LoadScene(_sceneName);
+    }
+}
